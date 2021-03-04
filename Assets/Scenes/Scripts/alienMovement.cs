8ebf04a216f387625c4c5246742f188d24dea1b7
Mai <dctmm222222@gmail.com>
2021-03-04 09:45:55 +0900

    alienMovement: alien chases player
    Alien chases the object named player at a constatnt speed that we
    hardcoded in the script.
    
    closes #9

---
+++ Assets/Scenes/Scripts/alienMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alienMovement : MonoBehaviour
+{
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.Find("Player");
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime * 2);
+        
+    }
+}
