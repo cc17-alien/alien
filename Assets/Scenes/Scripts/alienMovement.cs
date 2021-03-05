85c166e64e41304edf2ad73f98594de4497158de
Mai <dctmm222222@gmail.com>
2021-03-05 16:57:40 +0900

    WIP alienMovement: Alien moves depends on Player's Aggro
    Aggro is calculated using the noise variable of each
    player and the distance to the alien. The alien will
    chase the player with the highest Aggro value.
    
    resolving #10

--- Assets/Scenes/Scripts/alienMovement.cs
+++ Assets/Scenes/Scripts/alienMovement.cs
@@ -4,6 +4,40 @@ using UnityEngine;
 
 public class alienMovement : MonoBehaviour
 {
+    public float speed;
+    GameObject player;
+    playerNoise script;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        script = player.GetComponent<playerNoise>();
+    }
+
+    // Update is called once per frame
+    GameObject previousPlayer;
+    void Update()
+    {
+        GameObject[] arrayOfPlayerObjects = GameObject.FindGameObjectsWithTag("Player");
+        float previousPlayerAggro = 0;
+        float playerAggro;
+
+        
+        foreach (GameObject player in arrayOfPlayerObjects){
+            var distance = Vector3.Distance(transform.position, player.transform.position);
+            playerAggro = script.noise / distance;
+            if(playerAggro > previousPlayerAggro){
+                previousPlayerAggro = playerAggro;
+                previousPlayer = player;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, previousPlayer.transform.position, Time.deltaTime * speed);      
+    }    
+}
+
+/*
+public class alienMovement : MonoBehaviour
+{
     private GameObject player;
     public float speed;
     // Start is called before the first frame update
@@ -28,4 +62,4 @@ public class alienMovement : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, previousePlayer.transform.position, Time.deltaTime * speed);
         
     }
-}
+}*/
---
+++ Assets/Scenes/Scripts/playerNoise.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerNoise : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float noise;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
