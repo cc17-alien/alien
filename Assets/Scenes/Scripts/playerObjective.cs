84305d3e92c9da701c61ca683fd39e1c06c9f395
Yoko Tsuda <yoko20209@gmail.com>
2021-03-06 15:14:58 +0900

    Objectives spawned
    Made an Objective prefab. Made it a child of an empty obj to not
    sink into map when spawned.
    Added Ridgid Body component to Player prefab to be able to
    interact with Objective onTriggerEnter.
    Added SpawnOnMap component to Map obj to spawn Objectives with
    lat+long.
    
    resolving #15
    closes #11

---
+++ Assets/Scenes/Scripts/playerObjective.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerObjective : MonoBehaviour
+{
+    void OnTriggerStay(Collider collidedObj)
+    {
+        Debug.Log("Objective??");
+        if(collidedObj.gameObject.tag == "Objective"){
+            Debug.Log("Yes Objective");
+        }
+    }
+}
