67fc88b0f134a72fadd4d49648d2e2f7fc7fc1c1
Yoko Tsuda <yoko20209@gmail.com>
2021-03-07 11:52:48 +0900

    TaskUI: Displayable
    OpenTask button shows when player is near an Objective.
    TaskUI shows when OpenTask button is cliked.
    TaskUI disappears when QuitTask button is pressed, or Player is
    out of range of the Objective.
    
    resolving #20

--- Assets/Scenes/Scripts/playerObjective.cs
+++ Assets/Scenes/Scripts/playerObjective.cs
@@ -1,14 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerObjective : MonoBehaviour
 {
+    public GameObject OpenTaskButton;
+    public GameObject Task;
+
+    void Start()
+    {
+        // GameObject OpenTaskButton = GameObject.Find("OpenTaskButton");
+        // Button OpenTaskButton = OpenTaskButtonObj.GetComponent<Button>();
+        OpenTaskButton.SetActive(false);
+        Task.SetActive(false);
+    }
+
     void OnTriggerStay(Collider collidedObj)
     {
         Debug.Log("Objective??");
         if(collidedObj.gameObject.tag == "Objective"){
             Debug.Log("Yes Objective");
+            OpenTaskButton.SetActive(true);
         }
+    }
+
+    void OnTriggerExit(Collider collidedObj)
+    {
+        Debug.Log("Out?");
+        if(collidedObj.gameObject.tag == "Objective"){
+            Debug.Log("Out!!!!");
+            OpenTaskButton.SetActive(false);
+            Task.SetActive(false);
+        }
+    }
+
+    public void SetTaskActive()
+    {
+        Task.SetActive(true);
+    }
+    public void SetTaskInactive()
+    {
+        Task.SetActive(false);
     }
 }
