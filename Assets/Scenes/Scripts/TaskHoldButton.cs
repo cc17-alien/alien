4a4f7ef3436982d42c4d2d82905ebccfe05c306f
Yoko Tsuda <yoko20209@gmail.com>
2021-03-07 15:03:58 +0900

    Task for Objective Complete
    "Tap and Hold" button for given time (now 5 sec).
    "Completed!" screen after completion.
    Objectives change color when completed
    When the button is released before completion, the countdown stops,
    but does not recount. If you release at 3 sec, you will be able to
    resume from 3 sec even if you close the Task window and reopen it.
    
    resolving #20

---
+++ Assets/Scenes/Scripts/TaskHoldButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskHoldButton : MonoBehaviour
+{
+    
+    public int countDown;
+    public GameObject CountDownText;
+    public GameObject TaskComplete;
+    private UnityEngine.UI.Text textComponent;
+
+    public GameObject Objective;
+    public Material CompletionMaterial;
+
+    public void HandleHoldButton()
+    {
+        textComponent = CountDownText.GetComponent<UnityEngine.UI.Text>();
+        textComponent.text = countDown.ToString();
+        StartCoroutine(CountDown());
+    }
+
+    IEnumerator CountDown()
+    {
+        if(countDown > 0){
+            yield return new WaitForSeconds(1);
+            countDown -= 1;
+            textComponent.text = countDown.ToString();
+            StartCoroutine(CountDown());
+        }else if(countDown <= 0){
+            TaskComplete.SetActive(true);
+            Objective.GetComponent<MeshRenderer>().material = CompletionMaterial;
+        }
+       
+    }
+
+
+}
--- Assets/Scenes/Scripts/objectiveTask.cs
+++ Assets/Scenes/Scripts/objectiveTask.cs
@@ -1,26 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class playerObjective : MonoBehaviour
+public class objectiveTask : MonoBehaviour
 {
     public GameObject OpenTaskButton;
     public GameObject Task;
 
     void Start()
     {
-        // GameObject OpenTaskButton = GameObject.Find("OpenTaskButton");
-        // Button OpenTaskButton = OpenTaskButtonObj.GetComponent<Button>();
         OpenTaskButton.SetActive(false);
         Task.SetActive(false);
     }
 
     void OnTriggerStay(Collider collidedObj)
     {
-        Debug.Log("Objective??");
-        if(collidedObj.gameObject.tag == "Objective"){
-            Debug.Log("Yes Objective");
+        Debug.Log("Player??");
+        if (collidedObj.gameObject.tag == "Player")
+        {
+            Debug.Log("Yes Player");
             OpenTaskButton.SetActive(true);
         }
     }
@@ -28,9 +26,10 @@ public class playerObjective : MonoBehaviour
     void OnTriggerExit(Collider collidedObj)
     {
         Debug.Log("Out?");
-        if(collidedObj.gameObject.tag == "Objective"){
+        if (collidedObj.gameObject.tag == "Player" || collidedObj.gameObject.tag == "Eaten")
+        {
             Debug.Log("Out!!!!");
-            OpenTaskButton.SetActive(false);
+            OpenTaskButton.SetActive(false);    
             Task.SetActive(false);
         }
     }
