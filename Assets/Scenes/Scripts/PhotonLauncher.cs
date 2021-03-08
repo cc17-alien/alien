b26eeb08dc1fd0f815994e3a6c6462e4a2112ecb
Mai <dctmm222222@gmail.com>
2021-03-08 17:00:37 +0900

    JoinRoom Scene for MVP

---
+++ Assets/Scenes/Scripts/PhotonLauncher.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonLauncher : MonoBehaviourPunCallbacks
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Connect();
+    }
+
+    // Update is called once per frame
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    #region PunCallbacks
+
+    public override void OnConnectedToMaster() {
+        Debug.Log("OnConnectedToMaster called by PUN");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause) {
+        Debug.LogWarningFormat("OnDisconnected called by PUN with reason {0}", cause);
+    }
+
+    public override void OnJoinedRoom() {
+        Debug.Log("OnJoinedRoom called by PUN, user is in room");
+        SceneManager.LoadScene(1);
+    }
+
+    #endregion
+
+    #region public methods
+
+    public void Connect() {
+        if (!PhotonNetwork.IsConnected) {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    #endregion
+}
