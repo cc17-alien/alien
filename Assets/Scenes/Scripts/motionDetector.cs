99e4d04731670ee2cacd6f597097d60c59ec0aa7
Yoko Tsuda <yoko20209@gmail.com>
2021-03-08 21:38:36 +0900

    MotionDetector: Implemented Activate-Detector-Button
    A MotionDetectorButton UI on the Player that when pushed, activates
    the MotionDetector UI that will show AlienDots where Aliens are.
    Deactivates automatically in 2 sec for now.
    
    resolving #23

---
+++ Assets/Scenes/Scripts/motionDetector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class motionDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.activeSelf)
+        {
+            StartCoroutine(SetMotionDetectorInactive());
+        }
+    }
+
+
+    IEnumerator SetMotionDetectorInactive()
+    {
+        yield return new WaitForSeconds(2);
+        gameObject.SetActive(false);
+    }
+}
