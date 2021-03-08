cc02ec99587a0c687464be33a3add1262c71c744
Mai <dctmm222222@gmail.com>
2021-03-08 17:00:37 +0900

    JoinRoom Scene for MVP

---
+++ Assets/Scenes/Scripts/HandleJoinGame.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class HandleJoinGame : MonoBehaviourPunCallbacks
+{
+
+    private string roomName = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setRoomName(string value) {
+        roomName = value;
+        //Debug.Log(roomName);
+    }
+
+    public void JoinOrCreatePrivateRoom() {
+        Debug.Log(roomName);
+        PhotonNetwork.JoinOrCreateRoom(roomName, new RoomOptions(), null);
+    }
+
+    public override void OnJoinedRoom() {
+        Debug.Log("Joined Room!");
+    }
+}
