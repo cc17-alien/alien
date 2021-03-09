bae9784540945aa519ccd719b1f514b26a5df67b
Mai <dctmm222222@gmail.com>
2021-03-09 11:29:11 +0900

    GameManager: GameOver Scene for MVP
    Created allEaten scene to use as GameOver Scene for MVP
    BackToHome button goes to JoinRoom scene for now.
    PlayAgain button goes to BasicGame scene for now.
    
    resolving #21

---
+++ Assets/Scenes/Scripts/HandleGameOver.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+public class HandleGameOver : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayAgain() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void BackToHome() {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom() {
+        SceneManager.LoadScene(0);
+    }
+}
