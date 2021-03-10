ee3b7fef7b0bd1f99ad259fceb31b45a6797bdb9
Kevin Kee <kevinkee9@gmail.com>
2021-03-10 23:41:53 +0900

    playerNoise: noise reduce 1/sec if device level
    Every second, get tilt of device. If device is level (when holding it
    so screen is perpendicular to ground), noise reduces by 1 per second.
    
    resolving #16

--- Assets/Scenes/Scripts/playerNoise.cs
+++ Assets/Scenes/Scripts/playerNoise.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,12 +9,38 @@ public class playerNoise : MonoBehaviour
     public float noise;
     void Start()
     {
-        
+        StartCoroutine(NoiseReduction());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    float GetTilt()
+    {
+        Vector3 dir = Vector3.zero;
+
+        dir.x = Input.acceleration.x;
+        dir.z = Input.acceleration.z;
+
+        dir *= Time.deltaTime;
+
+        return Math.Abs(dir.x) + Math.Abs(dir.z);
+    }
+
+    IEnumerator NoiseReduction()
+    {
+        yield return new WaitForSeconds(1);
+
+        float tilt = GetTilt();
+
+        if (tilt < 0.005 && noise > 0)
+        {
+            noise -= 1;
+        }
+
+        StartCoroutine(NoiseReduction());
     }
 }
