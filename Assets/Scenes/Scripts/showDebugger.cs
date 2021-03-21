edfb5eb6e8f268047d6029eb7a241e5c2fa5e5c7
Kevin Kee <kevinkee9@gmail.com>
2021-03-21 23:57:49 +0900

    playerDebugger: toggle with fast tap
    Tapping five times in quick succession between objective button and map
    activates/deactivates debugger.
    
    resolving #63

---
+++ Assets/Scenes/Scripts/showDebugger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showDebugger : MonoBehaviour
+{
+    private int clickCount = 0;
+    private Canvas debugger; 
+
+    void Start()
+    {
+        debugger = gameObject.GetComponent<Canvas>();
+    }
+
+    IEnumerator CountClicks()
+    {
+        yield return new WaitForSeconds(5);
+        clickCount = 0;
+    }
+
+    public void ToggleDebugger()
+    {
+        clickCount += 1;
+
+        if (clickCount == 1)
+        {
+            StartCoroutine(CountClicks());
+        }
+
+        if (clickCount == 5)
+        {
+            if (debugger.enabled) debugger.enabled = false;
+            else debugger.enabled = true;
+            clickCount = 0;
+        }
+    }
+}
